9e3687ea0a1c3571486e3582ca2a094df0fadfa2
arne van kerschaver <arnevankerschaver@gmail.com>
2026-08-04 22:15:06 +0100

    "linux commit"

--- scripts/submit.py
+++ scripts/submit.py
@@ -2,6 +2,24 @@ import os
 import subprocess
 
 os.chdir("..")
+
+# add all
 subprocess.run([
-    "git add -A && git commit -m \"linux commit\" && git push"
+    "git",
+    "add",
+    "-A"
+], check=True)
+
+# commit
+subprocess.run([
+    "git",
+    "commit",
+    "-m",
+    "\"linux commit\""
+], check=True)
+
+# push
+subprocess.run([
+    "git",
+    "push"
 ], check=True)
